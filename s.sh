52d975fbdc818f849f5db8ae108ddadc003df569
MBA380 <mba380@outlook.com>
2025-02-25 21:27:31 -0500

    Updated naming

--- s.sh
+++
@@ -1,7 +0,0 @@
-python3 -m venv venv
-source venv/bin/activate
-pip install flask flask_cors
-pip freeze > requirements.txt
-pip install -r requirements.txt
-pip install google-genai
-python3 canify-backend/app.py
