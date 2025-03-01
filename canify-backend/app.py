2fb5c589687ab6827f7e386007f4d3c627c4b7cc
MBA380 <mba380@outlook.com>
2025-03-01 07:45:03 -0500

    Updated

--- canify-backend/app.py
+++ canify-backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
